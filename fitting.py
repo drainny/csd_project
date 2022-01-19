518332bc6bbbc544b86adbd31413d7545b49cec5
lorentz <lorentz@poincare.com>
2022-01-19 08:11:43 +0100

    Add write() method to Generator class Add read() method to fit class

--- fitting.py
+++ fitting.py
@@ -6,7 +6,27 @@ class fit:
     """
     Linear regression with minimal sum of squared errors.
     """
-    def __init__(self, x_n_max, x_n_min, y_n_max, y_n_min):
+    def __init__(self, x_n_max=None, x_n_min=None, y_n_max=None, y_n_min=None, filename=None):
+        if filename:
+            f = open(filename, 'r')
+            line = f.readline()
+            line = line.replace('(', '')
+            line = line.replace(')', '')
+            line = line.replace(',', '')
+            numbers = line.split(' ')[:-1]
+            self.y_n_max = [None for i in range(5)]
+            self.y_n_min = [None for i in range(5)]
+            self.x_n_max = [None for i in range(5)]
+            self.x_n_min = [None for i in range(5)]
+            for i in range(0, 5):
+                num_x_n = int(float(numbers[2*i]))
+                num_y_n = int(float(numbers[2*i+1]))
+                self.x_n_max[i] = (num_x_n + 1) * 2.5e-7
+                self.x_n_min[i] = num_x_n * 2.5e-7
+                self.y_n_max[i] = (num_y_n + 1) * 2.5e-7
+                self.y_n_min[i] = num_y_n * 2.5e-7
+            f.close()
+            return
         self.y_n_max = y_n_max
         self.y_n_min = y_n_min
         self.x_n_max = x_n_max
